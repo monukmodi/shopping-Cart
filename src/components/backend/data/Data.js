5d16dbb99b80c904d6f33125819b72bad709b3b8
monukmodi <monukr2012@gmail.com>
2021-09-24 06:28:12 +0530

    Dummy Data Added

---
+++ src/components/backend/data/Data.js
@@ -0,0 +1,69 @@
+const data = {
+    productItems:[
+        {
+            id:'1',
+            name:'Cat Tee Black T-Shirt',
+            size:'XS',
+            price:'10',
+            shipping_type: 'Free',
+            image:'./images/1.webp'
+        },
+        {
+            id:'2',
+            name:'Black T-Shirt',
+            size:'S',
+            price:'9',
+            shipping_type: 'Free',
+            image:'./images/2.webp'
+        },
+        {
+            id:'3',
+            name:'Green T-Shirt',
+            size:'M',
+            price:'8',
+            shipping_type: 'Paid',
+            image:'./images/1.webp'
+        }
+        ,
+        {
+            id:'4',
+            name:'Short T-Shirt',
+            size:'s',
+            price:'5',
+            shipping_type: 'Free',
+            image:'./images/2.webp'
+        },
+        {
+            id:'5',
+            name:'Long Red T-Shirt',
+            size:'XL',
+            price:'6.7',
+            shipping_type: 'Free',
+            image:'./images/1.webp'
+        },
+        {
+            id:'6',
+            name:'Extra Long Red T-Shirt',
+            size:'XXL',
+            price:'10.1',
+            shipping_type: 'Free',
+            image:'./images/2.webp'
+        },
+        {
+            id:'7',
+            name:'Black Shirt',
+            size:'XS',
+            price:'8.4',
+            shipping_type: 'Free',
+            image:'./images/1.webp'
+        },
+        {
+            id:'8',
+            name:'Dark Shirt',
+            size:'M',
+            price:'9.8',
+            shipping_type: 'Paid',
+            image:'./images/2.webp'
+        }
+    ]
+}
